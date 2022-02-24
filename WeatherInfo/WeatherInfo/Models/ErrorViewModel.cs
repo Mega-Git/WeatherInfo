efca1fe380c53dcc74ff8494ef3b7908c11e7406
Adam Pietrzak <pietrzakpraca@gmail.com>
2022-02-24 14:34:14 +0100

    New: MVC Project and solution

---
+++ WeatherInfo/WeatherInfo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeatherInfo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
